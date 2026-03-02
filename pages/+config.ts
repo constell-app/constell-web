63d0aff5340ae0dd384a040c51c7341b307def18
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-02 14:37:38 +0900

    fix: update head tags

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,13 +1,8 @@
 import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
 
-// Default config (can be overridden by pages)
-// https://vike.dev/config
-
 export default {
-  // https://vike.dev/head-tags
-  title: "My Vike App",
-  description: "Demo showcasing Vike",
-
+  title: "Constell Web",
+  description: "A web application for Constell, an AI-powered knowledge graph management system.",
   extends: [vikeReact],
 } satisfies Config;
